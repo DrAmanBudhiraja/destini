3ee7bfa04685f96cc7d75f793cb9ed3d04422739
DrAmanBudhiraja <amanbudhiraja@gmail.com>
2022-06-10 14:02:20 +0530

    Story choices added

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -52,6 +52,14 @@ class StoryBrain {
       storyNumber = 2;
     } else if (choiceNumber == 2 && storyNumber == 0) {
       storyNumber = 1;
+    } else if (choiceNumber == 1 && storyNumber == 1) {
+      storyNumber = 2;
+    } else if (choiceNumber == 2 && storyNumber == 1) {
+      storyNumber = 3;
+    } else if (choiceNumber == 1 && storyNumber == 2) {
+      storyNumber = 5;
+    } else if (choiceNumber == 2 && storyNumber == 2) {
+      storyNumber = 4;
     }
   }
 }
